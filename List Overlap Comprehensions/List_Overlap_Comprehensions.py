8a57e4e3520a884eff5ef7ba3c6d9f3e63509969
Hubertius <hubert.kopec@interia.pl>
2020-07-26 16:17:20 +0200

    List Overlap Comprehensions

---
+++ List Overlap Comprehensions/List_Overlap_Comprehensions.py
@@ -0,0 +1,17 @@
+"""
+
+Take two lists, say for example these two:
+
+	a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). 
+"""
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = []
+c = [element_1 for element_1 in a if element_1 in b]
+c = list(dict.fromkeys(c))
+print(c)
+
